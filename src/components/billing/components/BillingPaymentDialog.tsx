0ad5aeb811fbb02581a06f0db4158519ac08242b
JTP.Codeing <110913626+bigbossa@users.noreply.github.com>
2025-06-24 23:56:30 +0700

    plas URL

--- src/components/billing/components/BillingPaymentDialog.tsx
+++ src/components/billing/components/BillingPaymentDialog.tsx
@@ -51,8 +51,8 @@ const BillingPaymentDialog = ({
     if (!stripe) throw new Error("Stripe failed to initialize");
 
     const apiUrl = window.location.hostname === "localhost"
-      ? "http://localhost:3000"
-      : "http://172.20.10.6:3000";
+      ? "https://api-stripe-vercel.onrender.com"
+      : "https://api-stripe-vercel.onrender.com";
 
     console.log("Using API URL:", apiUrl);
 
